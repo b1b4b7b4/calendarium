e3628a3162679dd0db176134ede815b91253d6f7
b1b4b7b4 <danikfedorov41@gmail.com>
2026-01-11 08:36:52 +0200

    save

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,6 +28,9 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="preload" as="image" href="/bgs/image.png" />
+	<link rel="preload" as="image" href="/bgs/image1.png" />
+	<link rel="preload" as="image" href="/bgs/image3.png" />
 	<title>Bazi App</title>
 </svelte:head>
 
